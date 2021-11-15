01b23524df85334bb3eb8b2025c267c8e6006196
zahmatkesh <zm70525@gmail.com>
2021-11-15 14:38:38 +0330

    firstcommit

---
+++ components/sandbox.js
@@ -0,0 +1,36 @@
+import React, { useState }  from 'react';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+
+export default function Sandbox() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.boxOne}>one</Text>
+            <Text style={styles.boxTwo}>two</Text>
+            <Text style={styles.boxThree}>three</Text>
+            <Text style={styles.boxFour}>four</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+container: {
+    paddingTop: 40,
+    backgroundColor: '#ddd'
+},
+boxOne: {
+    backgroundColor: 'violet',
+    padding: 10
+},
+boxTwo: {
+    backgroundColor: 'gold',
+    padding: 10
+},
+boxThree: {
+    backgroundColor: 'coral',
+    padding: 10
+},
+boxFour: {
+    backgroundColor: 'skyblue',
+    padding: 10
+},
+})
